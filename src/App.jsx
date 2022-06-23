3b614f0f101b25870fe1694d9eb7523d332cef8c
Siddharth Khatri <siddxharth@gmail.com>
2022-06-24 00:56:42 +0530

    Add Input field with valid check

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import History from "./components/History.jsx";
+import UrlInput from "./components/UrlInput.jsx";
+
+function App() {
+  return (
+    <>
+      <UrlInput />
+      <History />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/UrlInput.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+const validateSubmit = (text) => {
+  if(text === ''){
+    alert("URL cannot be empty!")
+  }
+  if(text.includes(".")){
+    alert("Wow! This is actually some kind of a URL!")
+  }
+}
+
+export default function UrlInput() {
+  const [text, setText] = useState('');
+  return (
+  <div className='url-input-container'>
+    <input className="url-input" type="text" id="url-input" placeholder="Shorten URL" onChange={(e)=>{setText(e.target.value)}}/>
+    <button className="submit-btn" type="submit" onClick={() => validateSubmit(text)}>Go!</button>
+  </div>
+  )
+}
