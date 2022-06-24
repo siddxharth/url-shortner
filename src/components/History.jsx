53561b5df13d7320cfd6cb17f04408fa7cf471ec
Siddharth Khatri <siddxharth@gmail.com>
2022-06-24 10:30:43 +0530

    Create history view

---
+++ src/components/History.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const data = [
+    {
+        short: "srt.lnk/1",
+        long: "https://www.google.com"
+    },
+    {
+        short: "srt.lnk/2",
+        long: "https://www.yahoo.com"
+    }
+]
+
+export default function History() {
+    return (
+        <div className='history-container'>
+            <h3>History</h3>
+        </div>
+    )
+}
