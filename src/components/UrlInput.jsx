789f76a913fbb264a614d9cf5115512048b05b57
Siddharth Khatri <siddxharth@gmail.com>
2022-06-24 10:30:50 +0530

    Code formatted

--- src/components/UrlInput.jsx
+++ src/components/UrlInput.jsx
@@ -12,9 +12,11 @@ const validateSubmit = (text) => {
 export default function UrlInput() {
   const [text, setText] = useState('');
   return (
-  <div className='url-input-container'>
-    <input className="url-input" type="text" id="url-input" placeholder="Shorten URL" onChange={(e)=>{setText(e.target.value)}}/>
-    <button className="submit-btn" type="submit" onClick={() => validateSubmit(text)}>Go!</button>
+  <div>
+    <div className='url-input-container'>
+      <input className="url-input" type="text" id="url-input" placeholder="Shorten URL" onChange={(e)=>{setText(e.target.value)}}/>
+      <button className="submit-btn" type="submit" onClick={() => validateSubmit(text)}>Go!</button>
+    </div>
   </div>
   )
 }
