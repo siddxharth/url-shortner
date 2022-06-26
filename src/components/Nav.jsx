5f143c5f5b3e60095602bbecbb1ef0c87119441c
Siddharth Khatri <siddxharth@gmail.com>
2022-06-26 21:22:53 +0530

    Add alert to dummy links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,15 +6,15 @@ export default function Nav() {
         <div className='nav-left'>
             <Link className='nav-icon' to='/'>Shortly</Link>
             <ul className='Links'>
-                <li><Link className='nav-Link' to="/">Features</Link></li>
-                <li><Link className='nav-Link' to="/">Pricing</Link></li>
-                <li><Link className='nav-Link' to="/">Resources</Link></li>
+                <li><Link className='nav-Link' to="/" onClick={() => alert('This is a dummy link!')}>Features</Link></li>
+                <li><Link className='nav-Link' to="/" onClick={() => alert('This is a dummy link!')}>Pricing</Link></li>
+                <li><Link className='nav-Link' to="/" onClick={() => alert('This is a dummy link!')}>Resources</Link></li>
             </ul>
         </div>
         <div className='nav-right'>
             <ul className='login-signup'>
-                <li><Link className='nav-Link' to="/">Login</Link></li>
-                <li><Link className='nav-Link' to="/">SignUp</Link></li>
+                <li><Link className='nav-Link' to="/" onClick={() => alert('This is a dummy link!')}>Login</Link></li>
+                <li><Link className='nav-Link' to="/" onClick={() => alert('This is a dummy link!')}>SignUp</Link></li>
             </ul>
         </div>
     </nav>
