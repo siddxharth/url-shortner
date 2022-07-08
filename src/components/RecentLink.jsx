80804367d0a6a4afdefc7dc3bf3678b55cdb0adc
Siddharth Khatri <siddxharth@gmail.com>
2022-07-08 14:31:59 +0530

    Add copy button ui

--- src/components/RecentLink.jsx
+++ src/components/RecentLink.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default function RecentLink(props) {
+  const [copy, setCopy] = useState('Copy');
   return (
     <div className="recent-link-wrapper">
       <div className='recent-link-container'>
@@ -11,8 +12,8 @@ export default function RecentLink(props) {
         <span className='recent-link-right'>
           <span className='recent-link-short'>{props.short_link}</span>
           <CopyToClipboard text={props.short_link}
-          onCopy={() => {alert('Link copied to clipboard!')}}>
-          <button className='copy-btn'>Copy</button>
+          onCopy={() => setCopy('Copied')}>
+          <button className={`copy-btn-${copy}`}>{copy}</button>
         </CopyToClipboard>
         </span>
       </div>
